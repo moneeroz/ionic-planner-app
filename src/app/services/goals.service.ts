7a7debcb137350d0161f8beecaf065659ca6dbc9
Moneer <moneer.alromimah@gmail.com>
2023-04-21 17:12:11 -0600

    added the necessary services

--- src/app/services/goals.service.ts
+++ src/app/services/goals.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Igoal } from '../interfaces/igoal';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GoalsService {
+  apiUrl: string = 'http://localhost:3333/api/goals';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getGoals() {
+    return this.http.get<Igoal[]>(this.apiUrl);
+  }
+
+  getGoal(goal_id: string) {
+    return this.http.get<Igoal>(this.apiUrl + '/' + goal_id);
+  }
+
+  createGoal(goal_data: any) {
+    return this.http.post<Igoal>(this.apiUrl, goal_data);
+  }
+
+  updateGoalStatus(goal_id: string, status: string) {
+    return this.http.patch<Igoal>(
+      this.apiUrl + '/update-status/' + goal_id,
+      status,
+    );
+  }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Itodo } from '../interfaces/itodo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
+  apiUrl: string = 'http://localhost:3333/api/todos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getGoals() {
+    return this.http.get<Itodo[]>(this.apiUrl);
+  }
+
+  getGoal(todo_id: string) {
+    return this.http.get<Itodo>(this.apiUrl + '/' + todo_id);
+  }
+
+  createGoal(goal_data: any) {
+    return this.http.post<Itodo>(this.apiUrl, goal_data);
+  }
+
+  updateGoalStatus(todo_id: string, status: string) {
+    return this.http.patch<Itodo>(
+      this.apiUrl + '/update-status/' + todo_id,
+      status,
+    );
+  }
 }
