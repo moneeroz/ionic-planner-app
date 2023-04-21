13e8ddee5d47cfd79b80e93f4f058b534b50f968
Moneer <moneer.alromimah@gmail.com>
2023-04-21 17:53:58 -0600

    goals page now show goals from db

--- src/app/components/goal/goal.component.ts
+++ src/app/components/goal/goal.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { Igoal } from 'src/app/interfaces/igoal';
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.scss'],
+  standalone: true,
+  imports: [IonicModule, DatePipe, RouterModule],
 })
-export class GoalComponent  implements OnInit {
-
-  constructor() { }
+export class GoalComponent implements OnInit {
+  @Input() goal!: Igoal;
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- src/app/pages/goals/goals.page.ts
+++ src/app/pages/goals/goals.page.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
 import { IonicModule } from '@ionic/angular';
+import { GoalComponent } from 'src/app/components/goal/goal.component';
+import { Igoal } from 'src/app/interfaces/igoal';
+import { GoalsService } from 'src/app/services/goals.service';
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.page.html',
   styleUrls: ['./goals.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, GoalComponent, NgFor],
 })
 export class GoalsPage implements OnInit {
+  goals: Igoal[] = [];
 
-  constructor() { }
+  constructor(private goalsService: GoalsService) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.goalsService.getGoals().subscribe((goals) => (this.goals = goals));
   }
 
+  ngOnInit() {}
 }
