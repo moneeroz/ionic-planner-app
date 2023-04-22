fa5ec4c4e4cfe4853fa642f5c7b10143bd363ead
Moneer <moneer.alromimah@gmail.com>
2023-04-21 23:28:02 -0600

    add note works now

--- src/app/pages/add-note/add-note.page.ts
+++ src/app/pages/add-note/add-note.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule, NgFor } from '@angular/common';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { NotesService } from 'src/app/services/notes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-note',
   templateUrl: './add-note.page.html',
   styleUrls: ['./add-note.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, FormsModule, ReactiveFormsModule, NgFor],
 })
 export class AddNotePage implements OnInit {
+  noteForm;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private notesService: NotesService,
+    private router: Router,
+  ) {
+    const date = new Date();
+    date.setHours(date.getHours() - date.getTimezoneOffset() / 60);
 
-  ngOnInit() {
+    this.noteForm = formBuilder.group({
+      name: ['', Validators.required],
+      details: ['', Validators.required],
+      importance: ['', Validators.required],
+    });
   }
 
+  ngOnInit() {}
+
+  get nameFormControl() {
+    return this.noteForm.get('name');
+  }
+
+  get detailsFormControl() {
+    return this.noteForm.get('details');
+  }
+
+  get importanceFormControl() {
+    return this.noteForm.get('importance');
+  }
+
+  onSubmit() {
+    const goal_data = this.noteForm.value;
+
+    this.notesService
+      .createNote(goal_data)
+      .subscribe((result) => console.log(result));
+
+    this.router.navigateByUrl('notes');
+  }
 }
